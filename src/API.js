f74865891f5bf2a79c13d05886f3846ade3a326a
Mikhail Uljakov <pugumo68@gmail.com>
2022-08-22 05:04:45 +0700

    add mock object for API

--- src/API.js
+++ src/API.js
@@ -1,20 +1,56 @@
-import axios from 'axios';
+import axios from "axios";
 
 let axiosOpts = {
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json",
   },
   timeout: 60 * 1000,
-}
+};
 
-if (!process.env.production)
-{
+if (!process.env.production) {
   axiosOpts = {
     ...axiosOpts,
     withCredentials: true,
+  };
+}
+
+class MockResponse {
+  constructor(success, data) {
+    this.success = success;
+    this.data = data;
   }
 }
 
-const API = axios.create(axiosOpts);
+class MockApi {
+  constructor(axiosOpts) {
+    this.axiosOpts = axiosOpts;
+    this.bills = [];
+  }
+
+  parseBills(data) {
+    const bill = data?.data?.bill;
+    if (bill) {
+      this.bills.push(bill);
+    }
+    return new MockResponse(true, null);
+  }
+
+  async post(url, data, config) {
+    console.log(url, data, config);
+    let result = null;
+    switch (url) {
+      case "/ParseBills":
+        result = this.parseBills(data);
+        break;
+      default:
+        throw ([url, data, config], "Method is not mocked");
+    }
+    return { data: { d: { ...{ bills: this.bills }, ...result } } };
+  }
+}
+
+const API = process.env.production
+  ? axios.create(axiosOpts)
+  : new MockApi(axiosOpts);
 
 export default API;
